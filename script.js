02d70b667646725110109e02cfed3533ccb59a1c
rperroni <rperroni@fi.uba.ar>
2025-08-15 21:06:09 -0300

    agregar mas materias

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 let materias = [];
 let aprobadas = new Set();
-let cursando = new Set(); // nuevo set
+let cursando = new Set();
 let regulares = new Set();
+let carreraActual = "biomedica-2025"; // default
+
+/* Helpers para localStorage por carrera */
+function claveLS(tipo) {
+    return `${tipo}_${carreraActual}`;
+}
 
 function cargarProgreso() {
-    const data = localStorage.getItem("aprobadas");
-    if (data) {
-        aprobadas = new Set(JSON.parse(data));
-    }
-    const dataCursando = localStorage.getItem("cursando");
-    if (dataCursando) cursando = new Set(JSON.parse(dataCursando));
-    const dataRegulares = localStorage.getItem("regulares");
-    if (dataRegulares) regulares = new Set(JSON.parse(dataRegulares));
+    const data = localStorage.getItem(claveLS("aprobadas"));
+    aprobadas = data ? new Set(JSON.parse(data)) : new Set();
+
+    const dataCursando = localStorage.getItem(claveLS("cursando"));
+    cursando = dataCursando ? new Set(JSON.parse(dataCursando)) : new Set();
+
+    const dataRegulares = localStorage.getItem(claveLS("regulares"));
+    regulares = dataRegulares ? new Set(JSON.parse(dataRegulares)) : new Set();
 }
 
 function guardarProgreso() {
-    localStorage.setItem("aprobadas", JSON.stringify([...aprobadas]));
-    localStorage.setItem("cursando", JSON.stringify([...cursando]));
-    localStorage.setItem("regulares", JSON.stringify([...regulares]));
+    localStorage.setItem(claveLS("aprobadas"), JSON.stringify([...aprobadas]));
+    localStorage.setItem(claveLS("cursando"), JSON.stringify([...cursando]));
+    localStorage.setItem(claveLS("regulares"), JSON.stringify([...regulares]));
 }
 
 function actualizarEstado() {
     materias.forEach(m => {
         const elemento = document.getElementById(m.codigo);
         const rtfAdeudados = materias
-                .filter(x => !aprobadas.has(x.codigo))
-                .reduce((total, x) => total + (x.RTF || 0), 0);
+            .filter(x => !aprobadas.has(x.codigo))
+            .reduce((total, x) => total + (x.RTF || 0), 0);
 
         let puedeCursar;
-
-        if (m.codigo === "ingles"){
-            //modulo de ingles requiere 10 asignaturas aprobadas (+3 del ingreso)
+        // las materias modulo de ingles, pps, y pi tienen condiciones especiales de correlativas
+        if (m.codigo === "ingles") {
             puedeCursar = aprobadas.size > 13;
-        }else if (m.codigo === "PPS"){
-            // Práctica profesional supervisada: requiere adeudar <= 100 RTF
+        } else if (m.codigo === "PPS") {
             puedeCursar = rtfAdeudados <= 100;
-        } else if (m.codigo === "PI") { 
-            // Proyecto integrador: requiere adeudar <= 50 RTF
+        } else if (m.codigo === "PI") {
             puedeCursar = rtfAdeudados <= 50;
         } else {
-            // Caso normal: requiere correlativas aprobadas
             puedeCursar = m.correlativas.every(c => aprobadas.has(c) || regulares.has(c));
         }
-        //const puedeCursar = m.correlativas.every(c => aprobadas.has(c));
 
         if (aprobadas.has(m.codigo)) {
             elemento.className = "materia aprobada";
@@ -56,64 +57,65 @@ function actualizarEstado() {
             elemento.className = "materia bloqueada";
         }
 
-         // Actualizar tooltip del nombre
+        // Tooltip
         const nombreSpan = elemento.querySelector(".nombre-materia span");
-        if (aprobadas.has(m.codigo)) {
+        if (aprobadas.has(m.codigo)) { //al pasar el mouse sobre una materia aprobada sale el mensaje "desaprobar", que es su acción onclick
             nombreSpan.title = "Desaprobar";
-        } else if (cursando.has(m.codigo)) {
+        } else if (cursando.has(m.codigo)) { //hover mouse sobre una materia en curso muestra "regularizar"
             nombreSpan.title = "Regularizar";
-        } else if (puedeCursar || regulares.has(m.codigo)) {
+        } else if (puedeCursar || regulares.has(m.codigo)) { //sobre una materia disponible o regular dice "aprobar"
             nombreSpan.title = "Aprobar";
         } else {
-            nombreSpan.title = "Te faltan correlativas";
+            nombreSpan.title = "Te faltan correlativas"; //si la materia está bloqueada permite "aprobar" pero avisa que faltan las correlativas
         }
 
-        // Mostrar/ocultar botón de anotarse
+        // Botón anotarse
         const botonAnotarse = elemento.querySelector(".icono-anotarse");
         if (aprobadas.has(m.codigo) || (!puedeCursar) || regulares.has(m.codigo)) {
-            botonAnotarse.style.display = "none";
+            botonAnotarse.style.display = "none"; //si la materia está aprobada, no se puede cursar o está regular, no muestra el botón para anotarse
         } else {
-            botonAnotarse.style.display = "block"; // mostrar en cursando o disponible
-            botonAnotarse.innerHTML = cursando.has(m.codigo) 
-                ? '<i class="fa-solid fa-ban"></i>'  // desanotarse
-                : '<i class="fa-regular fa-pen-to-square"></i>';       // normal
-            botonAnotarse.title = cursando.has(m.codigo) 
-                ? "Abandonar"  // tachado
-                : "Anotarse";       // normal
+            botonAnotarse.style.display = "block";
+            botonAnotarse.innerHTML = cursando.has(m.codigo)
+                ? '<i class="fa-solid fa-ban"></i>' //botón desinscribirse si está en curso
+                : '<i class="fa-regular fa-pen-to-square"></i>'; //o inscribirse si no
+            botonAnotarse.title = cursando.has(m.codigo)
+                ? "Abandonar" //mensajes diferentes para cada caso arriba descrito
+                : "Anotarse";
         }
 
+        /*este bloque habilita el boton de anotarse/abandonar de acuerdo al estado de la materia*/ 
         const iconoAnotarse = elemento.querySelector(".icono-anotarse");
         if (iconoAnotarse) {
             if (elemento.classList.contains("disponible")) {
                 iconoAnotarse.classList.add("anotarse");
-                iconoAnotarse.classList.remove("desanotarse");
+                iconoAnotarse.classList.remove("abandonar");
             } else if (elemento.classList.contains("cursando")) {
-                iconoAnotarse.classList.add("desanotarse");
+                iconoAnotarse.classList.add("abandonar");
                 iconoAnotarse.classList.remove("anotarse");
             } else {
-                iconoAnotarse.classList.remove("anotarse", "desanotarse");
+                iconoAnotarse.classList.remove("anotarse", "abandonar");
             }
         }
     });
 }
 
-function toggleAprobada(codigo) {
+// esta función cambia el estado de una materia onclick
+function toggleEstado(codigo) {
     const materia = materias.find(m => m.codigo === codigo);
 
+    /* Si la materia está aprobada, para a disponible o bloqueada segun corresponda*/
     if (aprobadas.has(codigo)) {
         aprobadas.delete(codigo);
-    } else if (cursando.has(codigo)) {
-        if (materia.semestre === 0) {
-            // Semestre 0: ir directo a aprobada
+    } else if (cursando.has(codigo)) { // si la materia está en curso
+        if (materia.semestre === 0) { // y es del ingreso
             aprobadas.add(codigo);
-            cursando.delete(codigo);
+            cursando.delete(codigo); //pasa a aprobada
         } else {
-            // Semestres normales: pasar a regular
-            regulares.add(codigo);
+            regulares.add(codigo); // si no es del ingreso pasa a regular
             cursando.delete(codigo);
         }
-    } else { /* está regular */
-        regulares.delete(codigo);
+    } else {
+        regulares.delete(codigo); // otro caso (está regular) pasa a aprobada
         aprobadas.add(codigo);
     }
 
@@ -121,6 +123,7 @@ function toggleAprobada(codigo) {
     actualizarEstado();
 }
 
+// cambia el estado de la materia cuando se toca en anotarse/abandonar
 function toggleCursando(codigo) {
     if (cursando.has(codigo)) {
         cursando.delete(codigo);
@@ -131,69 +134,81 @@ function toggleCursando(codigo) {
     actualizarEstado();
 }
 
+function cargarCarrera(nombreCarrera) {
+    carreraActual = nombreCarrera;
 
-fetch("materias.json")
-    .then(res => res.json())
-    .then(data => {
-        materias = data;
-        cargarProgreso();
+    //carga el plan de estudios de la carrera seleccionada --> debe coincidir con el value de la lista del html, que debe coincidir con el nombre del archivo
+    fetch(`carreras/${nombreCarrera}.json`)
+        .then(res => res.json())
+        .then(data => {
+            materias = data;
 
-        const contenedor = document.getElementById("contenedor-semestres");
+            // limpiar antes de redibujar
+            const contenedor = document.getElementById("contenedor-semestres");
+            contenedor.innerHTML = "";
 
-        // Agrupar por semestre
-        const grupos = {};
-        materias.forEach(m => {
-            if (!grupos[m.semestre]) grupos[m.semestre] = [];
-            grupos[m.semestre].push(m);
-        });
+            cargarProgreso();
 
-        // Ordenar semestres y crear columnas
-        Object.keys(grupos).sort((a, b) => a - b).forEach(sem => {
-            const columna = document.createElement("div");
-            columna.className = "columna-semestre";
-
-            grupos[sem].forEach(m => {
-                const div = document.createElement("div");
-                div.id = m.codigo;
-                div.className = "materia";
-
-                // Nombre
-                const asignatura = document.createElement("div");
-                asignatura.classList.add("nombre-materia");
-
-                const nombre = document.createElement("span");
-                nombre.innerText = m.nombre;
-                nombre.onclick = () => toggleAprobada(m.codigo);
-
-                asignatura.appendChild(nombre);
-                div.appendChild(asignatura);
-
-                // Botón anotarse
-                const botonAnotarse = document.createElement("div");
-                botonAnotarse.className = "icono-anotarse";
-                botonAnotarse.onclick = e => {
-                    e.stopPropagation(); // no aprobar/desaprobar al hacer click en el ícono
-                    toggleCursando(m.codigo);
-                };
-                div.appendChild(botonAnotarse);
-
-                // Icono programa
-                if (m.programa_url) {
-                    const icono = document.createElement("a");
-                    icono.href = m.programa_url;
-                    icono.target = "_blank";
-                    icono.className = "icono-flotante";
-                    icono.title = "Ver programa";
-                    icono.classList.add("fa-regular", "fa-file");
-                    div.appendChild(icono);
-                }
-
-                columna.appendChild(div);
+            const grupos = {};
+            materias.forEach(m => {
+                if (!grupos[m.semestre]) grupos[m.semestre] = [];
+                grupos[m.semestre].push(m);
             });
 
-            contenedor.appendChild(columna);
-        });
+            Object.keys(grupos).sort((a, b) => a - b).forEach(sem => {
+                // crea las columnas de semestres
+                const columna = document.createElement("div");
+                columna.className = "columna-semestre";
 
-        actualizarEstado();
-    })
-    .catch(err => console.error("Error cargando materias:", err));
+                grupos[sem].forEach(m => {
+                    // crea las tarjetas de materias
+                    const div = document.createElement("div");
+                    div.id = m.codigo;
+                    div.className = "materia";
+
+                    const asignatura = document.createElement("div");
+                    asignatura.classList.add("nombre-materia");
+
+                    const nombre = document.createElement("span");
+                    nombre.innerText = m.nombre;
+                    nombre.onclick = () => toggleEstado(m.codigo);
+
+                    asignatura.appendChild(nombre);
+                    div.appendChild(asignatura);
+
+                    const botonAnotarse = document.createElement("div");
+                    botonAnotarse.className = "icono-anotarse";
+                    botonAnotarse.onclick = e => {
+                        e.stopPropagation();
+                        toggleCursando(m.codigo);
+                    };
+                    div.appendChild(botonAnotarse);
+
+                    // este bloque es el desplegable abajo de cada tarjeta que permite clickear para ver el programa de la materia
+                    if (m.programa_url) {
+                        const icono = document.createElement("a");
+                        icono.href = m.programa_url;
+                        icono.target = "_blank";
+                        icono.className = "icono-flotante fa-regular fa-file";
+                        icono.title = "Ver programa";
+                        div.appendChild(icono);
+                    }
+
+                    columna.appendChild(div);
+                });
+
+                contenedor.appendChild(columna);
+            });
+
+            actualizarEstado();
+        })
+        .catch(err => console.error("Error cargando materias:", err));
+}
+
+/* Listener del selector */
+document.getElementById("carrera").addEventListener("change", e => {
+    cargarCarrera(e.target.value);
+});
+
+/* Inicial */
+cargarCarrera(carreraActual);
