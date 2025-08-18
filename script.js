de80b6233e396aa63614053a3185679910706e51
rperroni <rperroni@fi.uba.ar>
2025-08-18 09:24:48 -0300

    lista completa de materias

--- script.js
+++ script.js
@@ -7,28 +7,215 @@ let carreraActual = "biomedica-2025"; // default
 // Lista de carreras para el menú personalizado
 const CARRERAS = [
     {
+        value: "biomedica-2005",
+        nombre: "Ingeniería Biomédica",
+        anio: "2005",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/ingenieria-biomedica/carrera-ingenieria-biomedica/plan-de-estudios-2005/"
+    },
+    {
         value: "biomedica-2025",
         nombre: "Ingeniería Biomédica",
         anio: "2025",
-        plan: "https://fcefyn.unc.edu.ar/documents/5190/Ing._Biom%C3%A9dica_Plan_de_estudio_2025.pdf"
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/ingenieria-biomedica/carrera-ingenieria-biomedica/plan-de-estudios-2025/"
     },
+//----------------------------------------------
+    {
+        value: "aeroespacial-2005",
+        nombre: "Ingeniería Aeroespacial",
+        anio: "2005",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-mecanica-aeronautica/carrera-ingenieria-aeroespacial/plan-de-estudios-2005/"
+    },
+    {
+        value: "aeroespacial-2025",
+        nombre: "Ingeniería Aeroespacial",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-mecanica-aeronautica/carrera-ingenieria-aeroespacial/plan-de-estudios-2025/"
+    },
+//----------------------------------------------
+    {
+        value: "agrimensura-2005",
+        nombre: "Ingeniería en Agrimensura",
+        anio: "2005",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-agrimensura/carrera-ingenieria-en-agrimensura/plan-de-estudios-2005/"
+    },
+    {
+        value: "agrimensura-2025",
+        nombre: "Ingeniería en Agrimensura",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-agrimensura/carrera-ingenieria-en-agrimensura/plan-de-estudios-2025/"
+    },
+//----------------------------------------------
+    {
+        value: "ambiental-2012",
+        nombre: "Ingeniería Ambiental",
+        anio: "2012",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-civil/carrera-ingenieria-ambiental/plan-de-estudios-2012/"
+    },
+    {
+        value: "ambiental-2025",
+        nombre: "Ingeniería Ambiental",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-civil/carrera-ingenieria-ambiental/plan-de-estudios-2025/"
+    },
+//----------------------------------------------
+    {
+        value: "civil-2005",
+        nombre: "Ingeniería Civil",
+        anio: "2005",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-civil/carrera-ingenieria-civil/plan-de-estudios-2005/"
+    },
+    {
+        value: "civil-2025",
+        nombre: "Ingeniería Civil",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-civil/carrera-ingenieria-civil/plan-de-estudios-2025/"
+    },
+//----------------------------------------------
+    {
+        value: "computacion-2005",
+        nombre: "Ingeniería en Computación",
+        anio: "2005",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/ingenieria-computacion/carrera-ingenieria-en-computacion/plan-de-estudios-2005/"
+    },
+    {
+        value: "computacion-2025",
+        nombre: "Ingeniería en Computación",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/ingenieria-computacion/carrera-ingenieria-en-computacion/plan-de-estudios-2025-/"
+    },
+//----------------------------------------------
+    {
+        value: "electronica-2005",
+        nombre: "Ingeniería Electrónica",
+        anio: "2005",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-electronica/carrera-ingenieria-electronica/plan-de-estudios-2005/"
+    },
+    {
+        value: "electronica-2025",
+        nombre: "Ingeniería Electrónica",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-electronica/carrera-ingenieria-electronica/plan-de-estudios-2025/"
+    },
+//----------------------------------------------
+    {
+        value: "electromecanica-2005",
+        nombre: "Ingeniería Electromecánica",
+        anio: "2005",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-mecanica-electricista/carrera-ingenieria-electromecanica/plan-de-estudios-2005-/"
+    },
+    {
+        value: "electromecanica-2025",
+        nombre: "Ingeniería Electromecánica",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-mecanica-electricista/carrera-ingenieria-electromecanica/plan-de-estudios-2025/"
+    },
+//----------------------------------------------
+    {
+        value: "industrial-2005",
+        nombre: "Ingeniería Industrial",
+        anio: "2005",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/ingenieria-industrial/carrera-ingenieria-industrial/plan-de-estudios-2005/"
+    },
+    {
+        value: "industrial-2025",
+        nombre: "Ingeniería Industrial",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/ingenieria-industrial/carrera-ingenieria-industrial/plan-de-estudios-2025/"
+    },
+//----------------------------------------------
+    {
+        value: "mecanica-2005",
+        nombre: "Ingeniería Mecánica",
+        anio: "2005",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-mecanica-electricista/carrera-ingenieria-mecanica/plan-de-estudios-2005/"
+    },
+    {
+        value: "mecanica-2025",
+        nombre: "Ingeniería Mecánica",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-mecanica-electricista/carrera-ingenieria-mecanica/plan-de-estudios-2025/"
+    },
+//----------------------------------------------
+    {
+        value: "quimica-2005",
+        nombre: "Ingeniería Química",
+        anio: "2005",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-quimica/carrera-ingenieria-quimica/plan-de-estudios-2005/"
+    },
+    {
+        value: "quimica-2025",
+        nombre: "Ingeniería Química",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-quimica/carrera-ingenieria-quimica/plan-de-estudios-2025/"
+    },
+//----------------------------------------------
+    {
+        value: "tme-1997",
+        nombre: "Tecnicatura en Mecánica Electricista",
+        anio: "1997",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-mecanica-electricista/carrera-tecnicatura-en-mecanica-electricista/"
+    },
+//----------------------------------------------
     {
         value: "tusd-2025",
         nombre: "Tecnicatura Universitaria en Sistemas Digitales",
         anio: "2025",
-        plan: "https://fcefyn.unc.edu.ar/documents/5905/Anexo_I_Grilla_de_Cursada-tusd25.pdf"
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-electronica/tecnicatura-universitaria-en-sistemas-digitales/"
     },
+    {
+        value: "tusd-2023",
+        nombre: "Tecnicatura Universitaria en Sistemas Digitales",
+        anio: "2023",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-electronica/tecnicatura-universitaria-en-sistemas-digitales/"
+    },
+//----------------------------------------------
     {
         value: "tuaqi-2025",
         nombre: "Tecnicatura Universitaria en Análisis Químico Industrial",
         anio: "2025",
-        plan: "https://fcefyn.unc.edu.ar/documents/5908/Anexo_I_TUAQUI_2025_-_Grilla_de_cursada.pdf"
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-quimica/carrera-tecnicatura-universitaria-en-analisis-quimico-industrial/"
     },
     {
-        value: "hidromteorologia-2024",
+        value: "tuaqi-2023",
+        nombre: "Tecnicatura Universitaria en Análisis Químico Industrial",
+        anio: "2023",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-quimica/carrera-tecnicatura-universitaria-en-analisis-quimico-industrial/"
+    },
+//----------------------------------------------
+    {
+        value: "hidrometeorologia-2024",
         nombre: "Licenciatura en Hidrometeorología",
         anio: "2024",
         plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-civil/carrera-licenciatura-en-hidrometeorologia/"
+    },
+//----------------------------------------------
+    {
+        value: "biologicas-2015",
+        nombre: "Ciencias Biológicas",
+        anio: "2015",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/biologia/carrera-ciencias-biologicas/"
+    },
+// ----------------------------------------------
+    {
+        value: "prof-cs-bio-1990",
+        nombre: "Profesorado en Ciencias Biológicas",
+        anio: "1990",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/biologia/carrera-profesorado-en-ciencias-biologicas/"
+    },
+//----------------------------------------------
+    {
+        value: "geologicas-2023",
+        nombre: "Ciencias Geológicas",
+        anio: "2023",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-geologia/ciencias-geologicas/"
+    },
+//----------------------------------------------
+
+    {
+        value: "constructor-2025",
+        nombre: "Constructor",
+        anio: "2025",
+        plan: "https://fcefyn.unc.edu.ar/facultad/secretarias/academica/escuelas/escuela-de-ingenieria-civil/carrera-constructor/plan-2025/"
     }
 ];
 
